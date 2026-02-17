4d8f77af475cee829e46c09f981eec7161387c8c
Majo Cuprik <marian.cuprik@icloud.com>
2026-02-17 09:16:15 +0100

    Revert "fix wrong xslt"
    This reverts commit cf2dc1c031adc55d9cf38fe303a66d8ab929cf18.

--- src/main/java/digital/slovensko/autogram/core/SigningParameters.java
+++ src/main/java/digital/slovensko/autogram/core/SigningParameters.java
@@ -73,9 +73,6 @@ public class SigningParameters {
         if (document.getMimeType() == null)
             throw new SigningParametersException("Dokument nemá definovaný MIME type", "Dokument poskytnutý na podpis nemá definovaný MIME type");
 
-        if (digestAlgorithm == null)
-            digestAlgorithm = DigestAlgorithm.SHA256;
-
         var extractedDocument = document;
         if (AutogramMimeType.isAsice(document.getMimeType()))
             extractedDocument = AsicContainerUtils.getOriginalDocument(document);
@@ -89,23 +86,23 @@ public class SigningParameters {
         var extractedDocumentMimeType = extractedDocument.getMimeType();
 
         if (eFormAttributes.containerXmlns() != null && eFormAttributes.containerXmlns().contains("xmldatacontainer")) {
+            if (digestAlgorithm == null) digestAlgorithm = DigestAlgorithm.SHA256;
+
             if (container == null) container = ASiCContainerType.ASiC_E;
 
             if (packaging == null) packaging = SignaturePackaging.ENVELOPING;
 
-            if (!AutogramMimeType.isXML(extractedDocumentMimeType) && !AutogramMimeType.isXDC(extractedDocumentMimeType))
+            if (AutogramMimeType.isXML(extractedDocumentMimeType) || AutogramMimeType.isXDC(extractedDocumentMimeType))
+                XDCValidator.validateXml(
+                        eFormAttributes.schema(), eFormAttributes.transformation(), extractedDocument,
+                        propertiesCanonicalization, digestAlgorithm, eFormAttributes.embedUsedSchemas());
+            else
                 throw new SigningParametersException("Nesprávny typ dokumentu", "Zadaný dokument nemožno podpísať ako elektronický formulár v XML Datacontaineri");
-        }
-
-        if (AutogramMimeType.isXDC(extractedDocumentMimeType))
-            XDCValidator.validateXml(
-                    eFormAttributes.schema(), eFormAttributes.transformation(), extractedDocument,
-                    propertiesCanonicalization, digestAlgorithm, eFormAttributes.embedUsedSchemas());
-        else if (eFormAttributes.containerXmlns() == null || !eFormAttributes.containerXmlns().contains("xmldatacontainer")) {
-            if (eFormAttributes.transformation() != null)
+        } else {
+            if (!AutogramMimeType.isXDC(extractedDocumentMimeType) && eFormAttributes.transformation() != null)
                 throw new SigningParametersException(XSLT_NO_XDC);
 
-                eFormAttributes = new EFormAttributes(null, null, null, null, null, null, false);
+            eFormAttributes = new EFormAttributes(null, null, null, null, null, null, false);
         }
 
         if (!plainXmlEnabled && (AutogramMimeType.isXML(extractedDocumentMimeType) || AutogramMimeType.isXDC(extractedDocumentMimeType)) && (eFormAttributes.transformation() == null))
