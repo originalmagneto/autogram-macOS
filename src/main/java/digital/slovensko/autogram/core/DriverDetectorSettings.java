cd4611cd1b8e0f323b2d2333a6b473ed967ac26c
Majo Cuprik <marian.cuprik@icloud.com>
2026-02-17 13:51:23 +0100

    Keep DriverDetectorSettings decoupled from missing language mixin

--- src/main/java/digital/slovensko/autogram/core/DriverDetectorSettings.java
+++ src/main/java/digital/slovensko/autogram/core/DriverDetectorSettings.java
@@ -1,6 +1,6 @@
 package digital.slovensko.autogram.core;
 
-public interface DriverDetectorSettings extends LanguageSettings {
+public interface DriverDetectorSettings {
     String getCustomKeystorePath();
     String getCustomPKCS11DriverPath();
 }
