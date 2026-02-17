e65119a0b1285dd75b232eba5689f311aeb82807
Majo Cuprik <marian.cuprik@icloud.com>
2026-02-17 16:03:34 +0100

    preserve XSLT_NO_XDC while validating XML datacontainer forms

--- src/main/java/digital/slovensko/autogram/core/SigningParameters.java
+++ src/main/java/digital/slovensko/autogram/core/SigningParameters.java
@@ -97,7 +97,10 @@ public class SigningParameters {
                 throw new SigningParametersException(WRONG_MIME_TYPE);
         }
 
-        if (AutogramMimeType.isXDC(extractedDocumentMimeType) || AutogramMimeType.isXML(extractedDocumentMimeType))
+        var isXdcEform = eFormAttributes.containerXmlns() != null
+                && eFormAttributes.containerXmlns().contains("xmldatacontainer");
+
+        if ((AutogramMimeType.isXDC(extractedDocumentMimeType) || AutogramMimeType.isXML(extractedDocumentMimeType)) && isXdcEform)
             XDCValidator.validateXml(
                     eFormAttributes.schema(), eFormAttributes.transformation(), extractedDocument,
                     propertiesCanonicalization, digestAlgorithm, eFormAttributes.embedUsedSchemas());
