2c5a229755bd8f9383075c3b62dcb17819b3268b
Majo Cuprik <marian.cuprik@icloud.com>
2026-09-01 19:35:35 +0200

    docs: prepare v0.2.3 release

--- Autogram/build_app.sh
+++ Autogram/build_app.sh
@@ -125,9 +125,9 @@ cat > "$CONTENTS/Info.plist" <<'PLIST'
         </dict>
     </array>
     <key>CFBundleVersion</key>
-    <string>0.2.2</string>
+    <string>0.2.3</string>
     <key>CFBundleShortVersionString</key>
-    <string>0.2.2</string>
+    <string>0.2.3</string>
     <key>CFBundlePackageType</key>
     <string>APPL</string>
     <key>CFBundleExecutable</key>
